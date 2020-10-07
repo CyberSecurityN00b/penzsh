942c71d8be5a48f52fc249c042e4a35440820a89
CyberSecN00b <51185020+CyberSecurityN00b@users.noreply.github.com>
2020-10-07 07:22:17 +0000

    More work, yet another commit to switch to the master branch.

--- penzsh.plugin.zsh
+++ penzsh.plugin.zsh
@@ -24,16 +24,28 @@ function chpwd_update_penzsh_vars() {
 function prompt_penzsh() {
 	if ( $PENZSH ) ; then
 		local pzsh_icon=""
+		local pzsh_text=""
 
 		case $PENZSH_OS in
 		windows) pzsh_icon="WINDOWS_ICON";;
 		linux) pzsh_icon="LINUX_ICON";;
 		bsd) pzsh_icon="FREEBSD_ICON";;
 		net) pzsh_icon="NETWORK_ICON";;
+		proxy) pzsh_icon="PROXY_ICON";;
 		*) pzsh_icon="VCS_UNTRACKED_ICON";;
 		esac
+
+		if [[ ! -f $PENZSH_PROXY_NET_DIR/.penzsh_proxy_net/proxy_pid ]] ; then
+			pzsh_text="${PENZSH_TARGET} !!! PROXY DOWN !!!"
+		elif ( $PENZSH_PROXY_HOST ) ; then
+			pzsh_text="${PENZSH_TARGET}(<[proxy]>${PENZSH_PROXY_NET_TARGET}<[proxy]>${PENZSH_TARGET})"
+		elif ( $PENZSH_PROXY_NET ) ; then
+			pzsh_text="${PENZSH_PROXY_NET_TARGET}(<[proxy]>${PENZSH_TARGET})"
+		else
+			pzsh_text="${PENZSH_TARGET}"
+		fi
 		
-		p10k segment -r -i ${pzsh_icon} -b red -t "$PENZSH_TARGET" 2>/dev/null
+		p10k segment -r -i ${pzsh_icon} -b red -t ${pzsh_text} 2>/dev/null
 	fi
 }
 
@@ -56,11 +68,12 @@ function update_current_penzsh_vars() {
 			export PENZSH=true
 			export PENZSH_DIR=$x
 			export PENZSH_DIR_META=$x/.penzsh
-			export PENZSH_LHOST=${$(ip route get $PENZSH_RHOST| awk '{print $7}'):-0.0.0.0}
+			export PENZSH_FIRST_TARGET=$x/.penzsh/target
 			break
 		elif [ `find "$x" -maxdepth 1 -name .penzsh_proxy_net -type d 2>/dev/null` ] ; then
 			export PENZSH_PROXY_NET=true
 			export PENZSH_PROXY_NET_DIR=$x
+			export PENZSH_PROXY_NET_TARGET=$x/.penzsh_proxy_net/target
 		elif [ `find "$x" -maxdepth 1 -name .penzsh_proxy_host -type d 2>/dev/null` ] ; then
 			export PENZSH_PROXY_HOST=true
 			export PENZSH_PROXY_HOST_DIR=$x
@@ -68,20 +81,20 @@ function update_current_penzsh_vars() {
 		x=`dirname "$x"`
 	done
 
-	if [ $PENZSH ] ; then
-		if [ $PENZSH_PROXY_HOST ] ; then
+	if ( $PENZSH ) ; then
+		if ( $PENZSH_PROXY_HOST ) ; then
 			export PENZSH_DIR=$PENZSH_PROXY_HOST_DIR
 			export PENZSH_DIR_META=$PENZSH_PROXY_HOST_DIR/.penzsh_proxy_host
 			export PENZSH_TARGET=$(cat $PENZSH_DIR_META/target)
 			export PENZSH_OS=$(cat $PENZSH_DIR_META/os)
-		elif [ $PENZSH_PROXY_NET ] ; then
+		elif ( $PENZSH_PROXY_NET ) ; then
 			export PENZSH_DIR=$PENZSH_PROXY_NET_DIR
 			export PENZSH_DIR_META=$PENZSH_PROXY_NET_DIR/.penzsh_proxy_net
 			export PENZSH_TARGET=$(cat $PENZSH_DIR_META/target)
 			export PENZSH_OS=$(cat $PENZSH_DIR_META/os)
 		else
-			export PENZSH_TARGET=$(cat $PENZSH_DIR_META/target
-			export PENZSH_OS=$(cat $PENZSH_DIR_META/os
+			export PENZSH_TARGET=$(cat $PENZSH_DIR_META/target)
+			export PENZSH_OS=$(cat $PENZSH_DIR_META/os)
 		fi
 
 		export PENZSH_RHOST=${PENZSH_TARGET}
@@ -98,6 +111,26 @@ function penzsh_echo() {
 
 function penzsh_cmd_do(){}
 function penzsh_cmd_info(){}
+
+function penzsh_create_host_dir() {
+	#1 - Root host directory
+	#2 - Target
+	
+	PENZSH_DIR="$1"
+	mkdir -p $PENZSH_DIR/.penzsh
+	echo $2 > $PENZSH_DIR/.penzsh/target
+	touch $PENZSH_DIR/.penzsh/notes
+	touch $PENZSH_DIR/.penzsh/todo
+	touch $PENZSH_DIR/.penzsh/os
+	mkdir -p $PENZSH_DIR/enum
+	mkdir -p $PENZSH_DIR/loot
+	mkdir -p $PENZSH_DIR/exploit
+	mkdir -p $PENZSH_DIR/privesc
+	mkdir -p $PENZSH_DIR/research
+	mkdir -p $PENZSH_DIR/server
+
+	update_current_penzsh_vars
+}
 
 function penzsh() {
 	local CMD=${1:-help}
@@ -189,15 +222,54 @@ function penzsh() {
 			;;
 		proxyhostnew)
 			if [ $PENZSH_PROXY_NET && !$PENZSH_PROXY_HOST ] ; then
+				local pz_c_target=""
+
+				if [ $2 ] ; then
+					pz_c_target=$2
+				else
+					read "target?Target: "
+					pz_c_target="$target"
+				fi
+
+				penzsh_create_host_dir "$(pwd)/$pz_c_target" "$pz_c_target"
 			else
-				penzsh_warn "This command is only available while in 'proxy_nets'!"
+				penzsh_warn "This command is only available in immediate 'proxy_nets' subfolders!"
 			fi
 			;;
 		proxyhostenum)
 			if [ $PENZSH_PROXY_NET && !$PENZSH_PROXY_HOST ] ; then
+				penzsh_echo "Performing basic host discovery, standby..."
+				for host in $(nmap -sn $PENZSH_PROXY_NET_TARGET -oG - | grep "Status: Up" | awk '{print $2}') ; do
+					penzsh_create_host_dir "$PENZSH_PROXY_NET_DIR/$host" "$host"
+				done
+				penzsh_echo "Basic host discovery completed. NOTE: Not all hosts may have been discovered!"
 			else
-				penzsh_warn "This command is only available while in 'proxy_nets'!"
+				penzsh_warn "This command is only available in immediate 'proxy_nets' subfolders!"
 			fi
+			;;
+		proxyconfig)
+			if [ $PENZSH_PROXY_NET && !$PENZSH_PROXY_HOST ] ; then
+				$PENZSH_PROGRAM_EDITOR $PENZSH_PROXY_NET_DIR/.penzsh_proxy_net/proxy.sh
+			else
+				penzsh_warn "This command is only available in immediate 'proxy_nets' subfolders!"
+			fi
+			;;
+		proxystart)
+			if [ $PENZSH_PROXY_NET && !$PENZSH_PROXY_HOST ] ; then
+				$PENZSH_PROXY_NET_DIR/.penzsh_proxy_net/proxy.sh &
+				echo $! > $PENZSH_PROXY_NET_DIR/.penzsh_proxy_net/proxy_pid
+			else
+				penzsh_warn "This command is only available in immediate 'proxy_nets' subfolders!"
+			fi
+			;;
+		proxystop)
+			if [ $PENZSH_PROXY_NET && !$PENZSH_PROXY_HOST ] ; then
+				kill $(cat $PENZSH_PROXY_NET_DIR/.penzsh_proxy_net/proxy_pid)
+			else
+				penzsh_warn "This command is only available in immediate 'proxy_nets' subfolders!"
+			fi
+			;;
+		proxynew)
 			;;
 		cmds)
 			echo "Custom Commands:"
@@ -209,7 +281,11 @@ function penzsh() {
 		*)
 			if [ -f $PENZSH_CUSTCMD_DIR/$1 ]; then
 				source $PENZSH_CUSTCMD_DIR/$1
-				penzsh_cmd_do ${@:2}
+				if ( $PENZSH_PROXY_NET ) ; THEN
+					proxychains penzsh_cmd_do ${@:2}
+				else
+					penzsh_cmd_do ${@:2}
+				fi
 			elif [ -f $PENZSH_CMD_DIR/$1 ]; then
 				source $PENZSH_CMD_DIR/$1
 				penzsh_cmd_do ${@:2}
@@ -224,6 +300,7 @@ function penzsh() {
 				echo -e "\t\tos:windows - Flag the target as a Windows machine."
 				echo -e "\tnote           - Save a note for later"
 				echo -e "\tnotes          - Read your notes for this target"
+				echo -e "\tproxynew       - Creates a new proxy"
 				echo -e "\ttodo           - Remind yourself of something"
 				echo -e "\ttodos          - See what you need to do for this target"
 				echo -e "\tupdate         - Updates the penzsh project, ONLY IF YOU GIT CLONED IT!"
@@ -231,8 +308,12 @@ function penzsh() {
 
 				if [ $PENZSH_PROXY_NET && !$PENZSH_PROXY_HOST ] ; then
 					echo -e "========== PROXY NETWORK =========="
+					echo -e "\tproxyconfig    - Configures the proxy for this CIDR."
 					echo -e "\tproxyhostnew   - Creates single proxy-host directory."
 					echo -e "\tproxyhostenum  - Creates proxy-host directories after host enumeration."
+					echo -e "\tproxystart     - Starts the proxy configured for this CIDR."
+					echo -e "\t\t\t(Note: For multiple proxies, make sure to start them in the correct order.)"
+					echo -e "\tproxystop      - Stops the proxy configured for this CIDR."
 					echo -e ""
 				fi
 
@@ -254,28 +335,26 @@ function penzsh() {
 	else
 		case $CMD in
 		create)
-			mkdir -p .penzsh
-			PENZSH_DIR=`pwd`
-			if [ $2 ] ; then
-				echo $2 > .penzsh/target
-			else
-				read "target?Target: "
-				echo $target > $PENZSH_DIR/.penzsh/target
-			fi
-			touch $PENZSH_DIR/.penzsh/notes
-			touch $PENZSH_DIR/.penzsh/todo
-			touch $PENZSH_DIR/.penzsh/os
-			mkdir -p $PENZSH_DIR/enum
-			mkdir -p $PENZSH_DIR/loot
-			mkdir -p $PENZSH_DIR/exploit
-			mkdir -p $PENZSH_DIR/privesc
-			mkdir -p $PENZSH_DIR/research
-			mkdir -p $PENZSH_DIR/server
+			local pz_c_name=""
+			local pz_c_target=""
 
-			update_current_penzsh_vars
+			if [ $1 ] ; then
+				pz_c_name = $1
+			else
+				read "name?Project Name: "
+			fi
+
+			if [ $2 ] ; then
+				pz_c_target = $2
+			else
+				read "target?Project Target: "
+			fi
+
+			penzsh_create_host_dir "$(pwd)/$pz_c_name" "$pz_c_target"
+
 			;;
 		*)
-			penzsh_echo "This is not a penzsh project. Please run 'penzsh create' to make this the root directory of a penzsh project."
+			penzsh_echo "This is not a penzsh project. Please run 'penzsh create' to create a penzsh project directory here."
 		esac
 	fi
 
